d3fde1f96bddd2b9a0cc99b99cbf5b0ebd0cc267
Shubha-KA <shubhaambareesh@gmail.com>
2026-04-28 02:17:37 +0530

    feat: implement custom DB and MQ metrics for Prometheus

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const { sequelize } = require('./config/db');
+const { register, metricsMiddleware } = require('./config/metrics');
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/user.routes');
@@ -6,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(metricsMiddleware);
 
 // Routes
 app.use('/api/users', userRoutes);
@@ -15,4 +18,13 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'UP' });
 });
 
+app.get('/metrics', async (req, res) => {
+  const { recordDbMetrics } = require('./config/metrics');
+  recordDbMetrics(sequelize);
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
 module.exports = app;
+
+
